19a319bf0cabbcc614a42707c30c327ac9b3421a
Saurabh S <saurabh.satralkar7@gmail.com>
2022-11-26 15:30:57 +0100

    Package details

--- 02TUT/dir.js
+++ 02TUT/dir.js
@@ -6,3 +6,10 @@ fs.mkdir("./new", (err) => {
     console.log("Directory created");
 }) 
 }
+
+if(fs.existsSync('./new')) {
+    fs.rmdir("./new", (err) => {
+        if (err) throw err;
+        console.log("Directory removed");
+    }) 
+    }
