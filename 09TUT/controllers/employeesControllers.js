7b7248bdf845620c251835093cefa6f7ef02574b
Saurabh S <saurabh.satralkar7@gmail.com>
2022-12-22 19:11:22 +0100

    Updated the employeeController

--- 09TUT/controllers/employeesControllers.js
+++ 09TUT/controllers/employeesControllers.js
@@ -21,22 +21,38 @@ const createNewEmployee = (req, res) => {
     }
 
     data.setEmployees([...data.employees, newEmployee]);
-    res.json(data.employees);
+    res.status(201).json(data.employees);
 }
 
 const updateEmployee = (req,res) => {
-    res.json({
-        "firstname": req.body.firstname,
-        "lastname": req.body.lastname
-    });
+    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+    if (!employee) {
+        return res.status(400).json({"message" : `Employee ID ${req.body.id} not found`});
+    }
+    if(req.body.firstname) employee.firstname = req.body.firstname;
+    if(req.body.lastname) employee.lastname = req.body.lastname;
+    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+    const unsortedArray = [...filteredArray, employee];
+    data.setEmployees(unsortedArray.sort((a,b)=> a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
+    res.json(data.employees);
 }
 
 const deleteEmplyee = (req, res) => {
-    res.json({"id": req.body.id});
+    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+    if (!employee) {
+        return res.status(400).json({"message" : `Employee ID ${req.body.id} not found`});
+    }
+    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+    data.setEmployees([...filteredArray]);
+    res.json(data.employees);
 }
 
 const getEmployee = (req, res) => {
-    res.json({"id": req.params.id });
+    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+    if (!employee) {
+        return res.status(400).json({"message" : `Employee ID ${req.body.id} not found`});
+    }
+    res.json(data.employee);
 }
 
 module.exports = {
