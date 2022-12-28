d0751ca64ba1ff361578d64f686d981fe7f62dbd
Saurabh S <saurabh.satralkar7@gmail.com>
2022-12-28 11:35:04 +0100

    Mongoose Data Schema created

---
+++ 14TUT/routes/api/employees.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const employeesController = require("../../controllers/employeesControllers");
+// const verifyJWT = require("../../middleware/verifyJWT");
+const ROLES_LIST = require("../../config/roles_list");
+const verifyRoles = require("../../middleware/verifyRoles");
+
+
+router.route("/")
+    .get(employeesController.getAllEmployees)
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.createNewEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmplyee);
+
+    router.route("/:id")
+        .get(employeesController.getEmployee);
+
+module.exports = router;
