d317df3064e6a8302a09141d95cc2faadb748205
Saurabh S <saurabh.satralkar7@gmail.com>
2023-01-04 11:24:35 +0100

    Async CRUD begin

---
+++ 15TUT/controllers/logoutController.js
@@ -0,0 +1,25 @@
+const User = require("../model/User");
+
+const handleLogout = async (req, res) => {
+    // On client, also delete the accessToken
+
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(204); // No content
+    const refreshToken = cookies.jwt;
+
+    // Is request token in DB?
+    const foundUser = await User.findOne({refreshToken}).exec();
+    if(!foundUser) {
+        res.clearCookie("jwt", {httpOnly: true });
+        return res.sendStatus(204); 
+    }  
+    // Delete refreshToken in DB
+    foundUser.refreshToken = "";
+    const result = await foundUser.save();
+    console.log(result);
+
+    res.clearCookie("jwt", {httpsOnly: true}); // secure: true - only serves on https
+    res.sendStatus(204);
+}
+
+module.exports = { handleLogout };
