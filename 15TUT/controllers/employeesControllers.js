ad95d70d8f0d8ef7fd27172f9c4802c333fdcf81
Saurabh S <saurabh.satralkar7@gmail.com>
2023-01-04 11:42:49 +0100

    Updated all controllers to be Async

--- 15TUT/controllers/employeesControllers.js
+++ 15TUT/controllers/employeesControllers.js
@@ -1,19 +1,24 @@
-const data = {
-    employees: require("../model/employees.json"),
-    setEmployees: function (data) { this.employees = data }
+const Employee = require("../model/Employee");
 
-};
-
-
-const getAllEmployees = (req, res) => {
-    res.json(data.employees);
+const getAllEmployees = async (req, res) => {
+   const employees = await Employee.find();
+   if(!employees) return res.status(204).json({'message': 'No Employees found'});
+   res.json(employees);
 }
 
-const createNewEmployee = (req, res) => {
-    const newEmployee = {
-        id: data.employees[data.employees.length - 1].id + 1 || 1,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname
+const createNewEmployee = async (req, res) => {
+    if (!req?.body?.firstname || !req?.body?.lastname) {
+        return res.status(400).json({'message':'First and last names are required'})
+    }
+
+    try {
+        const result = await Employee.create({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname
+        });
+        res.status(201).json(result);
+    }catch(err) {
+        console.error(err);
     }
 
     if (!newEmployee.firstname || !newEmployee.lastname) {
@@ -24,33 +29,39 @@ const createNewEmployee = (req, res) => {
     res.status(201).json(data.employees);
 }
 
-const updateEmployee = (req,res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+const updateEmployee = async (req,res) => {
+   if(!req?.body?.id){
+    return res.status(400).json({"message" : `ID parameter is required.`});
+   }
+   const employee = await Employee.findOne({_id: req.body.id}).exec();
     if (!employee) {
-        return res.status(400).json({"message" : `Employee ID ${req.body.id} not found`});
+        return res.status(204).json({"message" : `No Employee ID ${req.body.id} matches`});
     }
     if(req.body.firstname) employee.firstname = req.body.firstname;
     if(req.body.lastname) employee.lastname = req.body.lastname;
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    const unsortedArray = [...filteredArray, employee];
-    data.setEmployees(unsortedArray.sort((a,b)=> a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
-    res.json(data.employees);
+    
+    const result = await employee.save();
+    
+    res.json(result);
 }
 
-const deleteEmplyee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+const deleteEmployee = async (req, res) => {
+    if(!req?.body?.id) return res.status(400).json({'message': 'Employee ID required'})
+
+    const employee =  await Employee.findOne({_id: req.body.id}).exec();
     if (!employee) {
-        return res.status(400).json({"message" : `Employee ID ${req.body.id} not found`});
+        return res.status(204).json({"message" : `No Employee ID ${req.body.id} matches`});
     }
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    data.setEmployees([...filteredArray]);
-    res.json(data.employees);
+    const result = await employee.deleteOne({_id: req.body.id});
+   
+    res.json(result);
 }
 
-const getEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.params.id));
+const getEmployee = async (req, res) => {
+    if(!req?.params?.id) return res.status(400).json({'message': 'Employee ID required'});
+    const employee = await Employee.findOne({_id: req.body.id}).exec();
     if (!employee) {
-        return res.status(400).json({"message" : `Employee ID ${req.body.id} not found`});
+        return res.status(204).json({"message" : `No Employee ID ${req.body.id} matches`});
     }
     res.json(employee);
 }
@@ -59,6 +70,6 @@ module.exports = {
     getAllEmployees,
     createNewEmployee,
     updateEmployee,
-    deleteEmplyee,
+    deleteEmployee,
     getEmployee
 }
